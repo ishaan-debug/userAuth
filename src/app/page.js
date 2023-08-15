1e7f4edc3d9923f24019c9a96f14040a0c54a74c
ishaan-debug <65190665+ishaan-debug@users.noreply.github.com>
2023-08-16 02:59:05 +0530

    signup and login

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
+  return (    <main className={styles.main}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
