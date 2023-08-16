ced8d77c9b6ccf4849898111adfe9def3e22910a
ishaan-debug <65190665+ishaan-debug@users.noreply.github.com>
2023-08-17 00:10:04 +0530

    Update profile router error

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 
